b2d2dad174bbb36b9baff4a27070e255524a3f3d
Harold-debug <geumtchengh@gmail.com>
2021-11-27 17:57:52 +0100

    pushing environment variables

--- next.config.js
+++ next.config.js
@@ -7,4 +7,8 @@ module.exports = {
       "avatars.githubusercontent.com",
     ],
   },
+  env: {
+    FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
+    FACEBOOK_CLIENT_SECRET: process.env.FACEBOOK_CLIENT_SECRET,
+  },
 };
